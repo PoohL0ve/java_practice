aa58d50ab60a78c6bfd968d666858b9dfcd73ca7
PoohL0ve <screwed06@gmail.com>
2023-06-12 16:47:54 -0400

    "Simple for loop"

---
+++ Hello/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+    public static void main(String[] args) {
+        String her = "Ade";
+        String blank = " ";
+        String feeling = "rejected";
+        String someone = "you";
+        String sheDid = her + blank + feeling + blank + someone;
+
+        // looping the string
+        for (int check = 0; check < 5; check += 1) {
+            System.out.println(sheDid);
+        }
+    }
+}
