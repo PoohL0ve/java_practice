a1378b8745e495b9772f222fd10ee5f7236eae84
PoohL0ve <screwed06@gmail.com>
2023-06-12 20:37:26 -0400

    "Using a method and the Scanner class to test the Fermat Theorem"

---
+++ fermat_last_theorem/src/Fermat.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+/**
+ * Fermat last theorem stated that a^n + b^n = c^n only when n <= 2
+ * This program is testing to see if that's the case
+ * */
+public class Fermat {
+    public static void main(String[] args) {
+        // Using Scanner class to collect 4 integers
+        System.out.println("Enter for integers to be tested");
+        Scanner number = new Scanner(System.in);
+        int numA = number.nextInt();
+        int numB = number.nextInt();
+        int numC = number.nextInt();
+        int checkN = number.nextInt();
+        // function with the user defined integers as arguments
+        findTruth(numA, numB, numC, checkN);
+    }
+    // Method to test the theorem
+    public static void findTruth(int a, int b, int c, int n) {
+        //String shock;
+        double valueA = Math.pow(a, n);
+        double valueB = Math.pow(b, n);
+        double valueC = Math.pow(c, n);
+        System.out.println(valueC);
+        double addSome = valueA + valueB;
+        int testNum = 2;
+
+        // condition to test the statement
+        if (addSome == valueC && n > testNum) {
+            //shock = "Guess the theory needs to be evaluated";
+            System.out.println("Guess the theory needs to be evaluated");
+        } else if (n <= testNum && addSome == valueC) {
+            //shock = "Holy shit, the theory stands";
+            System.out.println("Holy shit, the theory stands");
+        } else {
+            System.out.println("The theory is flawed");
+        }
+    }
+}
