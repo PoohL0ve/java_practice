730be5e8ce9071c3c85595a7dd51cce171322f13
PoohL0ve <screwed06@gmail.com>
2023-10-19 16:12:37 -0400

    First window. It's empty Added JLabel and JTextField

--- FirstSwing/src/TryingSwing.java
+++ FirstSwing/src/TryingSwing.java
@@ -5,8 +5,24 @@ public class TryingSwing extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(400, 400);
         setVisible(true);
+        setLayout(null);
+        setLocationRelativeTo(null); // centers the window
+
+        // call the method that contains all components
+        testingComponents();
+    }
+    private void testingComponents() {
+        JLabel myTest = new JLabel();
+        myTest.setText("Test window!");
+        myTest.setBounds(40, 20, 100, 30);
+        add(myTest);
+
+        JTextField textBox = new JTextField();
+        textBox.setBounds(160, 20, 100, 30);
+        add(textBox);
     }
     public static void main(String[] args) throws Exception {
+        //new TryingSwing();
         Runnable initFrame = new Runnable() {
             public void run() {
                 new TryingSwing();
