e19cad4d0a68418c7fb942623657216a7755bf50
PoohL0ve <screwed06@gmail.com>
2024-06-16 01:26:09 -0400

    First set of Data Structures and Algorithms. Class taken through Study.com

--- dsa_most/src/SimpleArrays.java
+++ dsa_most/src/SimpleArrays.java
@@ -7,33 +7,29 @@ public class SimpleArrays {
         int[] numbers = {45, 12, 85, 32, 89, 39, 69, 44, 42, 1, 6, 8};
         int[] values = {56, 3, 34, 76, 12, 69, 7, 33, 11, 64, 45, 80};
 
-        System.out.println(Arrays.toString(insertionSort(numbers)));
+        // System.out.println(Arrays.toString(insertionSort(numbers)));
         /* The java.util.Arrays library has a sort method
         *  which perform Dual-Pivot Quick Sort similar to an
         *  insertion sort method, that is quicker to use.
         *  Also, range sorting is possible */
-        Arrays.sort(values, 3, 9); // the last index is not included
-        System.out.println("Range Sort: " + Arrays.toString(values));
+
+        // Arrays.sort(values, 3, 9); // the last index is not included
+        //System.out.println("Range Sort: " + Arrays.toString(values));
 
         /* For the binary search method to work, the array has to be
         *  sorted first. It returns a negative value if the number
         *  be found. */
-       int biSearch =  Arrays.binarySearch(numbers, 39);
-        System.out.println("Binary Search on numbers: " + biSearch);
+       // int biSearch =  Arrays.binarySearch(numbers, 39);
 
-        String classList[] = {"John", "Jean","James"};
+        Arrays.sort(numbers);
+        System.out.println("The value 32 is found at index: " +
+                recursiveBinarySearch(numbers, 0, numbers.length - 1, 32));
 
-        String graduatesList[] = {"John", "Jean", "James"};
+        System.out.println("Using exponential search *****");
+        Arrays.sort(values);
+        System.out.println("The value 56 is at position: " + expoSearch(values, values.length, 56));
 
-        if(Arrays.equals(classList, graduatesList)) {
 
-            System.out.println("All the students in this class are graduating");
-
-        } else {
-
-            System.out.println("Some of the students are not graduating.");
-
-        }
     }
 
     /**
@@ -56,5 +52,54 @@ public class SimpleArrays {
             }
         }
         return values;
+    }
+
+    /**
+     * A binary search method that looks for a value by continuously
+     * cutting the array in half until the value is found.
+     * @param numbers a sort array of integers
+     * @param begin the starting position
+     * @param end the last position in the array
+     * @param key the value to be found; it's position
+     * @return the position of the key value
+     */
+    public static int recursiveBinarySearch(int[] numbers, int begin, int end, int key) {
+        System.out.println("Searching for the index of: " + key);
+        if (begin < end) {  // if it's false -1 will be returned
+            int middle = begin + (end - begin) / 2;
+            if (key < numbers[middle]) {
+                return recursiveBinarySearch(numbers, begin, middle, key);
+            } else if (key > numbers[middle]) {
+                return recursiveBinarySearch(numbers, middle + 1, end, key);
+            } else {
+                return middle;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * An exponential search method that uses 3 steps to find a value
+     * and calculates the time complexity as O(log n) for the first
+     * binary search.
+     * @param numbers an array of integers
+     * @param size the size of the array
+     * @param key the value position to be found
+     * @return the position of the key value
+     */
+    public static int expoSearch(int[] numbers, int size, int key) {
+        // Step 1: check if the value is at the first position
+        if (numbers[0] == key) {
+            return 0;
+        }
+
+        // Step 2: Create the range for the binary search
+        int index = 1;  // start with one because of the first step
+        while (index < size && numbers[index] <= key) {
+            index *= 2;  // to ensure that we are working with even numbers to divide
+        }
+
+        // Step 3: Perform the binary search
+        return Arrays.binarySearch(numbers, (index / 2), Math.min(index, size), key);
     }
 }
